a9f830e8071488dd1a3e9baa5c11da383bd88f66
Scott <sbawden12@gmail.com>
2018-09-15 17:53:23 -0600

    Remade Script for character moving.

--- Game_Fall2018/Assets/Script Folder/Character_Move.cs
+++ Game_Fall2018/Assets/Script Folder/Character_Move.cs
@@ -7,15 +7,43 @@ public class Character_Move : MonoBehaviour {
 	public int MoveSpeed;
 	private float JumpHeight;
 
+//Player Grounded variables
+	public Transform GroundCheck;
+	public float GroundCheckRadius;
+	public LayerMask WhatIsGround;
+	private bool Grounded;
+
 	// Use this for initialization
-	void Start () 
-	{
-		print("Hello World!");
+	void Start () {
+	
 	}
 	
+
+	void FixedUpdate () {
+		Grounded = Physics2D.OverlapCircle(GroundCheck.position, GroundCheckRadius, WhatIsGround);
+	}
+
 	// Update is called once per frame
-	void Update () 
-	{
-		
+	void Update () {
+
+		// This code makes the character jump
+		if(Input.GetKeyDown (KeyCode.Space)&& Grounded){
+			Jump();
+		}
+
+		// This code makes the character move from side to side using the A&D keys
+		if(Input.GetKey (KeyCode.D)){
+			GetComponent<Rigidbody2D>().velocity = new Vector2(MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+			
+		}
+		if(Input.GetKey (KeyCode.A)){
+			GetComponent<Rigidbody2D>().velocity = new Vector2(-MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+			
+		}
+
+	}
+
+	public void Jump(){
+		GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, JumpHeight);
 	}
 }
