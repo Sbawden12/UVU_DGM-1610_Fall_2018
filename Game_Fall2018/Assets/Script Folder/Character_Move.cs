bfd0c7ba58aa7f94a0eced36531b69c5967bcf28
Scott <sbawden12@gmail.com>
2018-09-24 17:20:16 -0600

    Modified move script and started to add a score manager. Added double jump.

--- Game_Fall2018/Assets/Script Folder/Character_Move.cs
+++ Game_Fall2018/Assets/Script Folder/Character_Move.cs
@@ -1,17 +1,20 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class Character_Move : MonoBehaviour {
 
 	public int MoveSpeed;
 	public float JumpHeight;
+	private bool DoubleJump;
 
 //Player Grounded variables
 	public Transform GroundCheck;
 	public float GroundCheckRadius;
 	public LayerMask WhatIsGround;
 	private bool Grounded;
+
+// move velocity
+	private float MoveVelocity;
 
 	// Use this for initialization
 	void Start () {
@@ -31,15 +34,27 @@ public class Character_Move : MonoBehaviour {
 			Jump();
 		}
 
+		// Double jump
+		if(Grounded)	
+			DoubleJump = false;
+
+		if(Input.GetKeyDown (KeyCode.Space)&& !DoubleJump && !Grounded){
+			Jump();
+			DoubleJump = true;
+		}
+		//Non-Slide Player
+		MoveVelocity = ;
+
 		// This code makes the character move from side to side using the A&D keys
 		if(Input.GetKey (KeyCode.D)){
 			GetComponent<Rigidbody2D>().velocity = new Vector2(MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
-			
+			MoveVelocity = MoveSpeed;
 		}
 		if(Input.GetKey (KeyCode.A)){
 			GetComponent<Rigidbody2D>().velocity = new Vector2(-MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
-			
+			MoveVelocity = -MoveSpeed;
 		}
+		GetComponent<Rigidbody2D>().velocity = new Vector2(MoveVelocity, GetComponent<Rigidbody2D>().velocity.y);
 
 	}
 
