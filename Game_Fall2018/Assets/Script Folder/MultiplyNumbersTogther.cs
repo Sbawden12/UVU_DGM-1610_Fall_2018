5b41140f9a0140b17fb55e077bc455b4aa126a9a
Scott <sbawden12@gmail.com>
2018-10-08 16:08:08 -0600

    Added multiply function script.

---
+++ Game_Fall2018/Assets/Script Folder/MultiplyNumbersTogther.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultiplyNumbersTogther : MonoBehaviour {
+
+	public int Num1;
+	public int Num2;
+	public Total;
+
+	// Use this for initialization
+	void Start (){
+		CalcMultiply();
+		
+	}
+	
+	public void CalcMultiply() {
+		Multiply(Num1,Num2);
+	}
+
+	public void Multipley (int Num1, int Num2){
+		Total = Num1 * Num2;
+
+		print(Total);
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
