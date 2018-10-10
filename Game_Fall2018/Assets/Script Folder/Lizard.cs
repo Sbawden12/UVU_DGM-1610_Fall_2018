7fde27ba8091bd8a3a57aa7840a5f65e0735f570
Scott <sbawden12@gmail.com>
2018-10-10 17:17:10 -0600

    Added Eliminate character script and also added homework script for Inheritance script.

---
+++ Game_Fall2018/Assets/Script Folder/Lizard.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lizard : Inheritance {
+
+	public Color ScaleColor;
+	public int Size;
+	public bool Tail;
+	public string TypeOfLizard;
+	public string EyeType;
+
+}
