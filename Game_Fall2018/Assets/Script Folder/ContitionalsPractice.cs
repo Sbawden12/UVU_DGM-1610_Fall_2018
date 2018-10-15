d48f518b178906529a452240eb94de9973ba6752
Scott <sbawden12@gmail.com>
2018-10-15 17:18:19 -0600

    Added Enemy movement code to my game.

---
+++ Game_Fall2018/Assets/Script Folder/ContitionalsPractice.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContitionalsPractice : MonoBehaviour {
+
+	public string Baseball;
+
+	// Use this for initialization
+	void Start () {
+
+		if(Baseball == " Homerun ")
+			print("Hit a" + Baseball);
+
+		else if(Baseball == " Single ")
+			print("Hit a" + Baseball);
+			
+		else if(Baseball == " Double ")
+			print("Hit a" + Baseball);
+
+		else if(Baseball == " Triple ")
+			print("Hit a" + Baseball);
+
+		else
+			print("Strike Out" + Baseball);				
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
